110978f1ebd7cf9a2bafda1a9d114671e350ba66
Keosha Young <kyoung1012@gmail.com>
2022-10-30 11:33:23 -0400

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,10 @@
+username = input("Username >")
+password = input("Password >")
+if username == "Keosha" and password == "password":
+    print("Welcome, Keosha!")
+elif username == "Downy" and password == "password1":
+    print("Welcome, Downy!")
+elif username == "Reese" and password == "password2":
+    print("Welcome, Reese!")
+else:
+    print("It appears you don't have an account. Please Signup!")
